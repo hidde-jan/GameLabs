1b6868ed04220bcb199e5de2a0af32da8f8c2702
Hidde-Jan Jongsma <me@hidde-jan.nl>
2012-07-19 21:36:17 +0200

    Almost done

--- README.txt
+++ README.txt
@@ -16,27 +16,29 @@ point.  The problem is: this isn't Pong as you know it.  Enough said.
   the display for the opponent.  Your score shall be displayed at the
   top middle half of the left side of the board while the opponent's
   score shall be displayed at the top middle half of the right side of
-  the board.
+  the board. -- DONE
 
 * (1 point) As of right now, if you miss the ball (that is, the ball
   hits your wall on the left), it bounces.  Change the source code so
   that if the ball hits your wall, the opponent scores a point.
+  -- DONE
 
 * (1 point) Change the source code so that instead of receiving a
   point if your paddle hits the ball, you receive a point only if the
-  ball hits the right wall.
+  ball hits the right wall. -- DONE
 
 * (1 point) When the ball collides with a paddle, play a sound (find
-  one).
+  one). -- DONE
 
 * (1 point) Implement a second paddle on the right side. :-) Duh!
+  -- DONE
 
 * (1 point) Draw a line (e.g., a dotted black line, a solid red line)
-  dividing the playing surface, as in the real game of Pong.
+  dividing the playing surface, as in the real game of Pong. -- DONE
 
 * (1 point) Change the source so if the ball hits a wall (i.e.,
   someone scores), reset the ball to the middle of the playing surface
-  and move the ball to the player who got scored on.
+  and move the ball to the player who got scored on. -- DONE
 
 * (1 point) Change the source so if a player scores 11 points, the
   game is over.  That is, display who wins and prompt for a rematch.
@@ -44,7 +46,7 @@ point.  The problem is: this isn't Pong as you know it.  Enough said.
 
 * Either: (1 point) change your paddle to move using the "W" (up) and
 "S" (down) keys and use the UP and DOWN arrows for the opponent
-paddle, OR (1 point + 1 bonus point) make the opponent paddle play on
+paddle -- DONE, OR (1 point + 1 bonus point) make the opponent paddle play on
 its own. But don't make the opponent impossibly hard!
 
 * (1 point) Push your work to your GitHub account.
--- pong.py
+++ pong.py
@@ -3,10 +3,15 @@ import pygame, sys
 # Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
 PADDLE_START_X = 10
 PADDLE_START_Y = 20
+
 PADDLE_WIDTH = 10
 PADDLE_HEIGHT = 100
+
+OTHER_PADDLE_START_X = SCREEN_WIDTH - PADDLE_WIDTH - PADDLE_START_X
+
 BALL_SPEED = 10
 BALL_WIDTH_HEIGHT = 16
 
@@ -22,62 +27,95 @@ ball_speed = [BALL_SPEED, BALL_SPEED]
 
 # Your paddle vertically centered on the left side
 paddle_rect = pygame.Rect((PADDLE_START_X, PADDLE_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
+other_paddle_rect = pygame.Rect((OTHER_PADDLE_START_X, PADDLE_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
+
+# Line
+line_rect = pygame.Rect((SCREEN_WIDTH / 2 - 2, 10), (4, SCREEN_HEIGHT - 20))
 
 # Scoring: 1 point if you hit the ball, -5 point if you miss the ball
 score = 0
+other_score = 0
 
 # Load the font for displaying the score
 font = pygame.font.Font(None, 30)
 
+# Load the paddle sound.
+paddle_sound = pygame.mixer.Sound('paddle.wav')
+bounce_sound = pygame.mixer.Sound('v_bounce.wav')
+point_sound = pygame.mixer.Sound('point.wav')
+
 # Game loop
 while True:
-	# Event handler
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit(0)
-			pygame.quit()
-		# Control the paddle with the mouse
-		elif event.type == pygame.MOUSEMOTION:
-			paddle_rect.centery = event.pos[1]
-			# correct paddle position if it's going out of window
-			if paddle_rect.top < 0:
-				paddle_rect.top = 0
-			elif paddle_rect.bottom >= SCREEN_HEIGHT:
-				paddle_rect.bottom = SCREEN_HEIGHT
+    # Event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+            pygame.quit()
+        # Control the paddle with the mouse
+        elif event.type == pygame.MOUSEMOTION:
+            paddle_rect.centery = event.pos[1]
+            # correct paddle position if it's going out of window
+            if paddle_rect.top < 0:
+                paddle_rect.top = 0
+            elif paddle_rect.bottom >= SCREEN_HEIGHT:
+                paddle_rect.bottom = SCREEN_HEIGHT
 
-	# This test if up or down keys are pressed; if yes, move the paddle
-	if pygame.key.get_pressed()[pygame.K_UP] and paddle_rect.top > 0:
-		paddle_rect.top -= BALL_SPEED
-	elif pygame.key.get_pressed()[pygame.K_DOWN] and paddle_rect.bottom < SCREEN_HEIGHT:
-		paddle_rect.top += BALL_SPEED
-	elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
-		sys.exit(0)
-		pygame.quit()
-		
-	# Update ball position
-	ball_rect.left += ball_speed[0]
-	ball_rect.top += ball_speed[1]
+    # This test if up or down keys are pressed; if yes, move the paddle
+    if pygame.key.get_pressed()[pygame.K_w] and paddle_rect.top > 0:
+        paddle_rect.top -= BALL_SPEED
+    elif pygame.key.get_pressed()[pygame.K_s] and paddle_rect.bottom < SCREEN_HEIGHT:
+        paddle_rect.top += BALL_SPEED
+    if pygame.key.get_pressed()[pygame.K_UP] and other_paddle_rect.top > 0:
+        other_paddle_rect.top -= BALL_SPEED
+    elif pygame.key.get_pressed()[pygame.K_DOWN] and other_paddle_rect.bottom < SCREEN_HEIGHT:
+        other_paddle_rect.top += BALL_SPEED
+    elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
+        sys.exit(0)
+        pygame.quit()
 
-	# Ball collision with rails
-	if ball_rect.top <= 0 or ball_rect.bottom >= SCREEN_HEIGHT:
-		ball_speed[1] = -ball_speed[1]
-	if ball_rect.right >= SCREEN_WIDTH or ball_rect.left <= 0:
-		ball_speed[0] = -ball_speed[0]
+    # Update ball position
+    ball_rect.left += ball_speed[0]
+    ball_rect.top += ball_speed[1]
 
-	# Test if the ball is hit by the paddle; if yes reverse speed and add a point
-	if paddle_rect.colliderect(ball_rect):
-		ball_speed[0] = -ball_speed[0]
-		score += 1
-	
-	# Clear screen
-	screen.fill((255, 255, 255))
+    # Test if the ball hits an edge.
+    if ball_rect.left <= 0:
+        other_score += 1
+    elif ball_rect.right >= SCREEN_WIDTH:
+        score += 1
 
-	# Render the ball, the paddle, and the score
-	pygame.draw.rect(screen, (0, 0, 0), paddle_rect) # Your paddle
-	pygame.draw.circle(screen, (0, 0, 0), ball_rect.center, ball_rect.width / 2) # The ball
-	score_text = font.render(str(score), True, (0, 0, 0))
-	screen.blit(score_text, ((SCREEN_WIDTH / 2) - font.size(str(score))[0] / 2, 5)) # The score
-	
-	# Update screen and wait 20 milliseconds
-	pygame.display.flip()
-	pygame.time.delay(20)
+    # Ball collision with rails
+    if ball_rect.top <= 0 or ball_rect.bottom >= SCREEN_HEIGHT:
+        ball_speed[1] = -ball_speed[1]
+        bounce_sound.play()
+
+    if ball_rect.right >= SCREEN_WIDTH or ball_rect.left <= 0:
+        ball_speed[0] = -ball_speed[0]
+        ball_rect.left = SCREEN_WIDTH / 2
+        ball_rect.top = SCREEN_HEIGHT / 2
+        point_sound.play()
+
+    # Test if the ball is hit by the paddle; if yes reverse speed.
+    if paddle_rect.colliderect(ball_rect) or other_paddle_rect.colliderect(ball_rect):
+        ball_speed[0] = -ball_speed[0]
+        paddle_sound.play()
+
+    # Clear screen
+    screen.fill((255, 255, 255))
+
+    # Render the ball, the paddle, and the score
+    pygame.draw.rect(screen, (0, 0, 0), paddle_rect) # Your paddle
+    pygame.draw.rect(screen, (0, 0, 0), other_paddle_rect) # Other paddle
+
+    pygame.draw.rect(screen, (255, 0, 0), line_rect) # Line
+
+    pygame.draw.circle(screen, (0, 0, 0), ball_rect.center, ball_rect.width / 2) # The ball
+
+    score_text = font.render(str(score), True, (0, 0, 0))
+    screen.blit(score_text, ((SCREEN_WIDTH / 4) - font.size(str(score))[0] / 2, 5)) # The score
+
+    other_score_text = font.render(str(other_score), True, (0, 0, 0))
+    screen.blit(other_score_text, (int(SCREEN_WIDTH * 0.75) - font.size(str(other_score))[0] / 2, 5)) # The score
+
+    # Update screen and wait 20 milliseconds
+    pygame.display.flip()
+    pygame.time.delay(20)
